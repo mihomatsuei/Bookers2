358d05d14f5a05b46294e21eee59465729654837
松榮美帆 <>
2020-07-06 12:31:47 +0900

    [Finish]5-8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+# 新規登録後のリダイレクト先をbooks#indexにしてWelcome! You have signed up successfully.を表示
+    def after_sign_in_path_for(resource)
+      if @user.save #もしsign upが成功したら
+        flash[:notice] = "Welcome! You have signed up successfully." 
+        root_books_path  #　books#index
+      else #エラーが出たら
+        render :sign_up #sign upのままerrorを表示させたい
+    end
+end
+
 end
---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+    def index
+		@books = Book.all
+		@book = Book.new
+	end
+
+    def create
+    	@books = Book.all
+		@book = Book.new(book_params)
+		if @book.save
+		flash[:notice] = "Book was successfully created"
+		redirect_to book_path(@book.id) #投稿詳細画面(show)
+	    else
+	    render :index #投稿画面のままerrorを表示させたい(index)
+	    end
+	end
+
+	def show
+	    @book = Book.find(params[:id])
+
+	end
+
+	def edit
+		@book = Book.find(params[:id])
+
+	end
+
+	def update
+		@book = Book.find(params[:id])
+		if @book.update(book_params)
+		flash[:notice] = "Book was successfully updated"#
+		redirect_to book_path(@book.id)#show画面へ
+		else
+	    render :edit #編集画面のままerrorを表示させたい(edit)
+	    end
+	end
+
+	def destroy
+		book = Book.find(params[:id])
+		if book.destroy
+		flash[:notice] = "Book was successfully destroyed"
+		redirect_to books_path
+		end
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
+end
